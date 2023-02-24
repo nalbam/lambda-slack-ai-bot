2cd7840e4eb83ed78c27cfdb79f171780f985d6e
nalbam <me@nalbam.com>
2023-02-24 18:51:28 +0900

    fix lambda_handler

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # lambda-chatgpt-slack-bot
 
+## Install
+
+```bash
+$ brew install python@3.8
+
+$ npm install -g serverless
+
+$ sls plugin install -n serverless-python-requirements
+$ sls plugin install -n serverless-dotenv-plugin
+
+
+$ pip3 install --upgrade -r requirements.txt
+```
+
 ## Setup
 
 Setup a Slack app by following the guide at https://slack.dev/bolt-js/tutorial/getting-started
@@ -26,6 +40,19 @@ message.im
 
 In order to deploy the example, you need to run the following command:
 
+```bash
+$ sls deploy
 ```
-$ serverless deploy
+
+## Test
+
+```bash
+curl -X POST -H "Content-Type: application/json" \
+-d " \
+{ \
+    \"token\": \"Jhj5dZrVaK7ZwHHjRyZWjbDl\", \
+    \"challenge\": \"3eZbrw1aBm2rZgRNFdxV2595E9CY3gmdALWMmHkvFXO7tYXAYM8P\", \
+    \"type\": \"url_verification\" \
+}" \
+https://tnmahcbgth.execute-api.us-east-1.amazonaws.com/dev/slack/events
 ```
--- handler.py
+++ handler.py
@@ -1,5 +1,7 @@
 import os
 import openai
+import json
+
 # import openai_secret_manager
 
 from slack_bolt import App
@@ -12,26 +14,67 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 # Set up Slack API credentials
 SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 
 # Initialize Slack app
-app = App(token=SLACK_BOT_TOKEN)
+app = App(token=SLACK_BOT_TOKEN, signing_secret=SLACK_SIGNING_SECRET)
 
 
 # Define message event handler
-def handle_message(event, context):
-    # Retrieve user's message
-    message = event["body"]
+@app.event("message")
+def handle_message(event, say):
+    # Ignore messages from bots and the ChatGPT bot itself
+    is_bot_message = "bot_id" in event
+    is_from_chatgpt = event["bot_id"] == "B01F5J3U40E"
+    is_from_bot = (
+        event.get("user") == app.client.users_info(user=SLACK_BOT_TOKEN)["user"]["id"]
+    )
+    if is_bot_message and (is_from_chatgpt or is_from_bot):
+        return
+
     # Call OpenAI API to generate response
     response = openai.Completion.create(
         engine="davinci",
-        prompt=message,
+        prompt=event["text"],
         max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.5,
+        stream=True,  # Enable streaming mode
     )
 
     # Send response to Slack channel
-    app.client.chat_postMessage(
-        channel=event["headers"]["X-Slack-Channel-Id"], text=response.choices[0].text
-    )
+    response_text = ""
+    for message in response:
+        if "text" in message:
+            response_text += message["text"]
+        if "finish_reason" in message and message["finish_reason"] in [
+            "stop",
+            "max_tokens",
+        ]:
+            break
+    say(response_text)
+
+
+def lambda_handler(event, context):
+    body = json.loads(event["body"])
+    if "challenge" in body:
+        # Respond to the Slack Event Subscription Challenge
+        return {
+            "statusCode": 200,
+            "headers": {"Content-type": "application/json"},
+            "body": json.dumps({"challenge": body["challenge"]}),
+        }
+
+    # Initialize the Slack app
+    app.start()
+
+    # Handle the event
+    app.dispatch(body)
+
+    # Return a success message
+    return {
+        "statusCode": 200,
+        "headers": {"Content-type": "application/json"},
+        "body": json.dumps({"message": "Success"}),
+    }
